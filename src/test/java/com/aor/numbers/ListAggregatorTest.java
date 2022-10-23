510b9d364262b73ec8ff89a44f3ad4b52fc9f396
Mikhail <ermol24@icloud.com>
2022-10-23 15:18:31 +0100

    mutation test wee added

--- src/test/java/com/aor/numbers/ListAggregatorTest.java
+++ src/test/java/com/aor/numbers/ListAggregatorTest.java
@@ -8,7 +8,7 @@ import org.mockito.Mockito;
 import java.util.*;
 
 public class ListAggregatorTest {
-    List<Integer> list1, list2, list3, list4 = new ArrayList<>();
+    List<Integer> list1, list2, list3, list4, list5, list6, list7 = new ArrayList<>();
 
     @BeforeEach
     public void helper(){
@@ -16,6 +16,9 @@ public class ListAggregatorTest {
         list2 = Arrays.asList(7,4,3,2,3);
         list3 = Arrays.asList(1,1,8,7,1);
         list4 = Arrays.asList(1,2,3,4,5);
+        list5 = Arrays.asList(-5,-3,-1,0,1);
+        list6 = Arrays.asList(0,0,0);
+        list7 = Arrays.asList(3,3,3);
     }
 
     @Test
@@ -25,10 +28,17 @@ public class ListAggregatorTest {
         int sum2 = aggregator.sum(list2);
         int sum3 = aggregator.sum(list3);
         int sum4 = aggregator.sum(list4);
+        int sum5 = aggregator.sum(list5);
+        int sum6 = aggregator.sum(list6);
+        int sum7 = aggregator.sum(list7);
         Assertions.assertEquals(14, sum1);
         Assertions.assertEquals(19, sum2);
         Assertions.assertEquals(18, sum3);
         Assertions.assertEquals(15, sum4);
+        Assertions.assertEquals(-8, sum5);
+        Assertions.assertEquals(0, sum6);
+        Assertions.assertEquals(9, sum7);
+
     }
 
     @Test
@@ -38,10 +48,16 @@ public class ListAggregatorTest {
         int max2 = aggregator.max(list2);
         int max3 = aggregator.max(list3);
         int max4 = aggregator.max(list4);
+        int max5 = aggregator.max(list5);
+        int max6 = aggregator.max(list6);
+        int max7 = aggregator.max(list7);
         Assertions.assertEquals(5, max1);
         Assertions.assertEquals(7, max2);
         Assertions.assertEquals(8, max3);
         Assertions.assertEquals(5, max4);
+        Assertions.assertEquals(1, max5);
+        Assertions.assertEquals(0, max6);
+        Assertions.assertEquals(3, max7);
     }
 
     @Test
@@ -51,10 +67,16 @@ public class ListAggregatorTest {
         int min2 = aggregator.min(list2);
         int min3 = aggregator.min(list3);
         int min4 = aggregator.min(list4);
+        int min5 = aggregator.min(list5);
+        int min6 = aggregator.min(list6);
+        int min7 = aggregator.min(list7);
         Assertions.assertEquals(1, min1);
         Assertions.assertEquals(2, min2);
         Assertions.assertEquals(1, min3);
         Assertions.assertEquals(1, min4);
+        Assertions.assertEquals(-5, min5);
+        Assertions.assertEquals(0, min6);
+        Assertions.assertEquals(3, min7);
     }
 
     @Test
@@ -75,16 +97,35 @@ public class ListAggregatorTest {
                 Mockito.mock(GenericListDeduplicator.class);
         Mockito.when(dedupMockito4.deduplicate(Mockito.anyList(), Mockito.any())).thenReturn(Arrays.asList(1, 2, 3, 4, 5));
 
+        GenericListDeduplicator dedupMockito5 =
+                Mockito.mock(GenericListDeduplicator.class);
+        Mockito.when(dedupMockito5.deduplicate(Mockito.anyList(), Mockito.any())).thenReturn(Arrays.asList(-5, -3, -1, 0, 1));
+
+        GenericListDeduplicator dedupMockito6 =
+                Mockito.mock(GenericListDeduplicator.class);
+        Mockito.when(dedupMockito6.deduplicate(Mockito.anyList(), Mockito.any())).thenReturn(Arrays.asList());
+
+        GenericListDeduplicator dedupMockito7 =
+                Mockito.mock(GenericListDeduplicator.class);
+        Mockito.when(dedupMockito7.deduplicate(Mockito.anyList(), Mockito.any())).thenReturn(Arrays.asList());
+
         ListAggregator aggregator = new ListAggregator();
 
         int distinct1 = aggregator.distinct(list1, dedupMockito1);
         int distinct2 = aggregator.distinct(list2, dedupMockito2);
         int distinct3 = aggregator.distinct(list3, dedupMockito3);
         int distinct4 = aggregator.distinct(list4, dedupMockito4);
+        int distinct5 = aggregator.distinct(list5, dedupMockito5);
+        int distinct6 = aggregator.distinct(list6, dedupMockito6);
+        int distinct7 = aggregator.distinct(list7, dedupMockito7);
+
         Assertions.assertEquals(3, distinct1);
         Assertions.assertEquals(3, distinct2);
         Assertions.assertEquals(2, distinct3);
         Assertions.assertEquals(5, distinct4);
+        Assertions.assertEquals(5, distinct5);
+        Assertions.assertEquals(0, distinct6);
+        Assertions.assertEquals(0, distinct7);
     }
 
     @Test
--- src/test/java/com/aor/numbers/ListSorterTest.java
+++ src/test/java/com/aor/numbers/ListSorterTest.java
@@ -7,13 +7,15 @@ import org.junit.jupiter.api.Test;
 import java.util.*;
 
 public class ListSorterTest {
-    List<Integer> list1, list2, list3= new ArrayList<>();
+    List<Integer> list1, list2, list3, list4, list5 = new ArrayList<>();
 
     @BeforeEach
     public void helper(){
         list1 = Arrays.asList(1,2,4,2);
         list2 = Arrays.asList(7,6,4,3,1,1,1);
         list3 = Arrays.asList(1,3,2,4,9);
+        list4 = Arrays.asList(0);
+        list5 = Arrays.asList(-1,2,-5,0);
     }
 
     @Test
@@ -22,9 +24,13 @@ public class ListSorterTest {
         List<Integer> sorted1 = sorter.sort(list1);
         List<Integer> sorted2 = sorter.sort(list2);
         List<Integer> sorted3 = sorter.sort(list3);
+        List<Integer> sorted4 = sorter.sort(list4);
+        List<Integer> sorted5 = sorter.sort(list5);
 
         Assertions.assertEquals(Arrays.asList(1,2,2,4), sorted1);
         Assertions.assertEquals(Arrays.asList(1,1,1,3,4,6,7), sorted2);
         Assertions.assertEquals(Arrays.asList(1,2,3,4,9), sorted3);
+        Assertions.assertEquals(Arrays.asList(0), sorted4);
+        Assertions.assertEquals(Arrays.asList(-5,-1,0,2), sorted5);
     }
 }
